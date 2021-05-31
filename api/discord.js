cd4a0cfb25917f051c80f3c2b60123bf0bba6d39
Patrick Brennan <brennanpatrick20@gmail.com>
2021-05-31 11:28:36 -0400

    attempting to solve ratelimiting issues

--- api/discord.js
+++ api/discord.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
+const rateLimit = require('axios-rate-limit');
 
-const getUser = async (id) => {
-    try {
-        const response = await axios({
-            method: 'get',
-            url: `https://discord.com/api/users/${id}`,
+const http = rateLimit(axios.create({
+            baseURL: 'https://discord.com/api',
             headers: {
                 'Authorization':`Bot ${process.env.TOKEN}`,
             }
-        })
+        }
+    ), 
+    {
+        maxRequests: 15,
+        perMilliseconds: 1000,
+        maxRPS: 15,
+    }
+);
+
+const getUser = async (id) => {
+    try {
+        const response = await http.get(`/users/${id}`);
         return response;
     } catch (error) {
         return { data: { error: error.message } };
--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-//const cors = require('cors');
-const { getUser, getAvatar } = require('./discord');
+const { getUser } = require('./discord');
 
 const app = express();
 const api = express.Router();
@@ -15,7 +14,6 @@ api.get('/avatar/:id.:format', async (req, res) => {
     res.redirect(`https://cdn.discordapp.com/avatars/${userData.data.id}/${userData.data.avatar}.${req.params.format}${ (req.query.size) ? '?size=' + req.query.size : ''}`);
 })
 
-//app.user(cors());
 app.use('/api',api);
 
 module.exports = app;
