0cea8983ee368cdab3e50c1f5f9a1d538bd66f24
worstbby <97109094+worstbby@users.noreply.github.com>
2022-12-25 17:24:16 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,14 +14,18 @@ api.get('/avatar/:id.:format', async (req, res) => {
     res.redirect(`https://cdn.discordapp.com/avatars/${userData.data.id}/${userData.data.avatar}.${req.params.format}${ (req.query.size) ? '?size=' + req.query.size : ''}`);
 });
 
-api.get('/banner/:id.:format', async (req, res) => {
+api.get('/banner/:id.:format/:size?', async (req, res) => {
     const allowedFormats = ['webp', 'gif', 'png', 'jpg'];
     if (!allowedFormats.includes(req.params.format)) {
         return res.status(400).send({ error: 'Invalid format' });
     }
 
     const userData = await getUser(req.params.id);
-    res.redirect(`https://cdn.discordapp.com/banners/${userData.data.id}/${userData.data.banner}.${req.params.format}`);
+    let bannerUrl = `https://cdn.discordapp.com/banners/${userData.data.id}/${userData.data.banner}.${req.params.format}`;
+    if (req.params.size) {
+        bannerUrl += `?size=${req.params.size}`;
+    }
+    res.redirect(bannerUrl);
 });
 
 api.get('/badges/:id', async (req, res) => {
