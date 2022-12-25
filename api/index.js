be5e5caa1f58546d652bfb8d2ee67dfd781d53cb
worstbby <97109094+worstbby@users.noreply.github.com>
2022-12-25 17:15:10 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,9 +14,14 @@ api.get('/avatar/:id.:format', async (req, res) => {
     res.redirect(`https://cdn.discordapp.com/avatars/${userData.data.id}/${userData.data.avatar}.${req.params.format}${ (req.query.size) ? '?size=' + req.query.size : ''}`);
 });
 
-api.get('/banner/:id', async (req, res) => {
+api.get('/banner/:id.:format', async (req, res) => {
+    const allowedFormats = ['webp', 'gif', 'png', 'jpg'];
+    if (!allowedFormats.includes(req.params.format)) {
+        return res.status(400).send({ error: 'Invalid format' });
+    }
+
     const userData = await getUser(req.params.id);
-    res.redirect(`https://cdn.discordapp.com/banners/${userData.data.id}/${userData.data.banner}.png`);
+    res.redirect(`https://cdn.discordapp.com/banners/${userData.data.id}/${userData.data.banner}.${req.params.format}`);
 });
 
 api.get('/badges/:id', async (req, res) => {
