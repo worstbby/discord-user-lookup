f80cbbdb63ffc5fff22071cabe1d60dcde92b88c
worstbby <97109094+worstbby@users.noreply.github.com>
2022-12-25 16:42:02 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,13 +7,19 @@ const api = express.Router();
 api.get('/user/:id', async (req, res) => {
     const userData = await getUser(req.params.id);
     res.send(userData.data);
-})
+});
 
 api.get('/avatar/:id.:format', async (req, res) => {
     const userData = await getUser(req.params.id);
     res.redirect(`https://cdn.discordapp.com/avatars/${userData.data.id}/${userData.data.avatar}.${req.params.format}${ (req.query.size) ? '?size=' + req.query.size : ''}`);
-})
+});
 
-app.use('/api',api);
+api.get('/banner/:id', async (req, res) => {
+    const userData = await getUser(req.params.id);
+    res.redirect(`https://cdn.discordapp.com/banners/${userData.data.id}/${userData.data.banner}.png`);
+});
+
+app.use('/api', api);
 
 module.exports = app;
+
